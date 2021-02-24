95e8bcd9b9738045cf77a7490d8d48ff5b45b28c
compix <wladimirkroeker@googlemail.com>
2021-02-25 00:57:57 +0100

    Progress update support

--- actions/Action.py
+++ actions/Action.py
@@ -1,14 +1,27 @@
+from MetadataManagerCore.Event import Event
 from MetadataManagerCore.actions.ActionType import ActionType
+import math
 
 class Action(object):
     def __init__(self) -> None:
         super().__init__()
 
         self.linkedCollections = []
+        self.progressUpdateEvent = Event()
+        self.currentProgress = 0
 
     def execute(self):
         pass
 
+    def updateProgress(self, progress: float, progressMessage: str = None):
+        """Updates the progress of this action.
+
+        Args:
+            progress (float): Expected value in [0,1].
+        """
+        self.currentProgress = max(min(progress, 1.0), 0.0)
+        self.progressUpdateEvent(progress, progressMessage)
+        
     @property
     def actionType(self):
         return ActionType.GeneralAction
