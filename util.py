79f82f20dd85827c4cbb9ac93d685a13758f5e1f
Wladimir Kröker <wkroeker@meyle-mueller.de>
2022-06-03 16:50:17 +0200

    Add replaceGermanCharacters function to util

--- util.py
+++ util.py
@@ -10,3 +10,6 @@ def timeit(f):
         print('func:%r args:[%r, %r] took: %0.0fms' % (f.__name__, args, kw, (te-ts) * 1000.0))
         return result
     return wrap
+
+def replaceGermanCharacters(input: str):
+    return input.replace('ö', 'oe').replace('ü', 'ue').replace('ä', 'ae').replace('Ä', 'AE').replace('Ü', 'UE').replace('Ö', 'OE').replace('ß', 'ss').replace('ẞ', 'SS')
